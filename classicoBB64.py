c7aa332be35a1def3610a24b5d6345b40a3858f5
Ramon-UFSJ2023 <ramonsjdr70@aluno.ufsj.edu.br>
2025-11-13 11:33:38 -0300

    Funcionaaaa

--- classicoBB64.py
+++ classicoBB64.py
@@ -53,9 +53,10 @@ def VectorInterferenciaEve(vectorAlice, qtd_bits):
         else:
             if BaseSortEve == escolhaBases.Base_X:
                 resultadoEveObteu = rd.choice([escolhaQubit.QubitMinus,escolhaQubit.QubitMore])
+                bitSendBob = resultadoEveObteu
             else:
                 resultadoEveObteu = rd.choice([escolhaQubit.QubitZero,escolhaQubit.QubitOne])
-            bitSendBob = resultadoEveObteu
+                bitSendBob = resultadoEveObteu
 
         baseSendBob = BaseSortEve
         #vectorObtidoEve.append(resultadoEveObteu)
@@ -96,23 +97,62 @@ def amostragem(newVectorAlice, newVectorBob):
     tamanhoVector = len(newVectorAlice)
     tamanho_amostragem = mt.ceil(tamanhoVector*QTD_BITS_AMOSTRAGEM)
     vector_posi = rd.sample(range(tamanhoVector),k=tamanho_amostragem)
+    VectorAlicePosAmostragem=[]
+    VectorBobPosAmostragem = []
 
     for i in range(tamanho_amostragem):
         posicao = vector_posi[i]
-        if newVectorAlice[posicao].Qubit == newVectorBob[posicao].Qubit:
+        if newVectorAlice[posicao].Qubit == newVectorBob[posicao].Qubit: 
             numeroBitsCertos = numeroBitsCertos+1
-    taxa_erro = (tamanho_amostragem-numeroBitsCertos)/tamanho_amostragem
-    return taxa_erro, tamanho_amostragem
+    taxa_erro =(tamanho_amostragem-numeroBitsCertos)/tamanho_amostragem
+
+    for i in range(tamanhoVector):
+        if i != posicao:
+            VectorAlicePosAmostragem.append(newVectorAlice[i])
+            VectorBobPosAmostragem.append(newVectorBob[i])
+    return taxa_erro, tamanho_amostragem, numeroBitsCertos, VectorAlicePosAmostragem, VectorBobPosAmostragem
+
+def taxaDeErroGeral(vectorAlicePosAmostragem, vectorBobPosAmostragem):
+    bits_certos =0
+    tamanho_vector = len(vectorBobPosAmostragem)
+
+    for i in range(tamanho_vector):
+        if(vectorAlicePosAmostragem[i].Qubit == vectorBobPosAmostragem[i].Qubit):
+            bits_certos+=1
+    taxa_acerto_geral = bits_certos/tamanho_vector
+    taxa_erro_geral = (tamanho_vector-bits_certos)/tamanho_vector
+    return bits_certos, taxa_acerto_geral, taxa_erro_geral, tamanho_vector
+
+
 
 def FindQtdBits():
-    qtd_bits = 10
-    taxaDeErroSemCanal=0
+    qtd_bits = 256
+    taxaDeErroSemCanal=3
+    tamanhoAmostragem =0
+    numeroBitCertos =0
+
     while(taxaDeErroSemCanal>0.2):
         vectorAlice = gereVectorAlice(qtd_bits);
         vectorEveInterfere=VectorInterferenciaEve(vectorAlice, qtd_bits)
         vectorBob=MedicaoBob(vectorEveInterfere, qtd_bits)
         newVectorAlice,newVectorBob = comparacaoBase(vectorAlice, vectorBob, qtd_bits)
-        taxaDeErroSemCanal, tamanhoAmostragem= amostragem(newVectorAlice, newVectorBob)
+
+        taxaDeErroSemCanal, tamanhoAmostragem, numeroBitCertos, vectorAlicePosAmostragem, vectorBobPosAmostragem = amostragem(newVectorAlice, newVectorBob)
         qtd_bits= qtd_bits+2
-    print(qtd_bits)
-    print(tamanhoAmostragem)
+
+    bitsCertosGeral, taxaAcertoGeral, taxaDeErroGera, tamanhoVectorPosAmostragem = taxaDeErroGeral(vectorAlicePosAmostragem, vectorBobPosAmostragem)
+    print(f"Tamanho do vector: {qtd_bits}\n")
+    print(f"Tamanho Amostragem: {tamanhoAmostragem}")
+    print("Bits Certos da Amostra{numeroBitCertos}")
+    print("Taxa de acerto da amostragem: {:.4f}".format(numeroBitCertos/tamanhoAmostragem))
+    print("Taxa de erro da amostra: {:.4f}\n".format(taxaDeErroSemCanal))
+
+    print(f"Numero de Bits PosAmostragem: {tamanhoVectorPosAmostragem}")
+    print(f"Numero de Bits certos PosAmostragem: {bitsCertosGeral}")
+    print("Taxa de acerto Geral: {:.4f}".format(taxaAcertoGeral))
+    print("Taxa de Erro Geral: {:.4f}".format(taxaDeErroGera))
+
+
+def  main():
+    FindQtdBits()
+main()
